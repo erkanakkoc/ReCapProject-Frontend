a515762d4c03a25d9c4dde57fa44f128407d5b01
akkoc.erkan@gmail.com <akkoc.erkan@gmail.com>
2021-03-11 19:54:49 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarComponent,
+    ColorComponent,
+    NaviComponent,
+    BrandComponent,
+    CustomerComponent,
+    RentalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
